de8ee5fcc9da64132de336e807b42ca5672f3a47
Volodymyr Bonetskyy <volodymyr.bonetskyy@tieto.com>
2016-02-16 15:12:30 +0100

    Clicking added

--- src/2.4Selemium/sel.py
+++ src/2.4Selemium/sel.py
@@ -3,38 +3,36 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait # available since 2.4.0
 from selenium.webdriver.support import expected_conditions as EC # available since 2.26.0
 
-# import ConfigParser
-#
-# Config = ConfigParser.ConfigParser()
-# Config.read("params.ini")
-# HOST = str(Config.get('Section1', 'HOST'))
-# user = str(Config.get('Section1', 'user'))
-# password = str(Config.get('Section1', 'password'))
+import ConfigParser
+
+Config = ConfigParser.ConfigParser()
+Config.read("params.ini")
+customer_area = str(Config.get('Section1', 'customer_area'))
 
 # Create a new instance of the Firefox driver
 driver = webdriver.Firefox()
 
 # go to the google home page
-driver.get("http://www.google.com")
+driver.get("http://www.tieto.pl/")
 
 # the page is ajaxy so the title is originally this:
 print driver.title
 
 # find the element that's name attribute is q (the google search box)
-inputElement = driver.find_element_by_name("q")
-
-# type in the search
-inputElement.send_keys("cheese")
-
-# submit the form (although google automatically searches now without submitting)
-inputElement.submit()
-
-try:
-    # we have to wait for the page to refresh, the last thing that seems to be updated is the title
-    WebDriverWait(driver, 10).until(EC.title_contains("cheese"))
-
-    # You should see "cheese! - Google Search"
-    print driver.title
-
-finally:
-    driver.quit()
+inputElement = driver.find_element_by_id(customer_area)
+inputElement.click()
+# # type in the search
+# inputElement.send_keys("cheese")
+#
+# # submit the form (although google automatically searches now without submitting)
+# inputElement.submit()
+#
+# try:
+#     # we have to wait for the page to refresh, the last thing that seems to be updated is the title
+#     WebDriverWait(driver, 10).until(EC.title_contains("cheese"))
+#
+#     # You should see "cheese! - Google Search"
+#     print driver.title
+#
+# finally:
+#     driver.quit()
