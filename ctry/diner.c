5734872b62d20259f596c9b727f1433958112918
VolBon <vansup@gmail.com>
2016-05-13 15:01:27 +0200

    newsss

--- ctry/diner.c
+++ ctry/diner.c
@@ -4,7 +4,13 @@
 
 int main()
 {
-	printf("%i\n", INT_MAX);
+	printf("%i\n", CHAR_MAX);
+	printf("The value of INT_MIN is %i\n", CHAR_MIN);
+	printf("The value of FLT_MAX is %f\n", FLT_MAX);
+	printf("The value of FLT_MIN is %.50f\n", FLT_MIN);
+	printf("The value of FLT_MAX is %f\n", DBL_MAX);
+	printf("The value of FLT_MIN is %.50f\n", DBL_MIN);
+		
 	
 	return 0;
-	}
+}
