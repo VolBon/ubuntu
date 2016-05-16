45d0969e7a99330c81fb6089724668388925eb62
VolBon <vansup@gmail.com>
2016-05-16 13:55:57 +0200

    fish make

---
+++ ctry/fish.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+struct fish {
+const char *name;
+const char *species;
+int teeth;
+int age;
+};
+
+void catalog(struct fish f)
+{
+printf("%s is a %s with %i teeth. He is %i\n",
+f.name , f.species , f.teeth , f.age );
+}
+
+void label(struct fish f)
+{
+printf("%s %s %i %i\n",
+f.name , f.species , f.teeth , f.age );
+}
+
+int main()
+{
+
+struct fish snappy = {"Snappy", "Piranha", 69, 4};
+catalog(snappy);
+label(snappy);
+return 0;
+}
